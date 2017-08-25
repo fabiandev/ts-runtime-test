7d73f0a12a2885a43d239115b6da5746869f252e
Fabian Pirklbauer <fabian.pirklbauer@gmail.com>
2017-08-25 23:23:12 +0200

    Loading indicator improvements.

--- docs/assets/loading.js
+++ docs/assets/loading.js
@@ -5,21 +5,26 @@ this.loading.status = [];
 this.loading._show = debounce(function() {
   if (this.loading.status.length > 0) {
     document.getElementById('loading').style.display = 'block';
+    document.getElementById('loading-text').style.display = 'block';
   }
 }.bind(this), 50);
 
 this.loading._hide = debounce(function() {
   if (this.loading.status.length === 0) {
     document.getElementById('loading').style.display = 'none';
+    document.getElementById('loading-text').style.display = 'none';
   }
 }.bind(this), 50);
 
-this.loading.show = function showLoading() {
-  this.loading.status.push(0);
+this.loading.show = function showLoading(url) {
+  this.loading.status.push(url);
   this.loading._show();
 }.bind(this);
 
-this.loading.hide = function hideLoading() {
-  this.loading.status.pop();
-  this.loading._hide();
+this.loading.hide = function hideLoading(url) {
+  var index = this.loading.status.indexOf(url);
+  if (index > -1) {
+    this.loading.status.splice(index, 1);
+    this.loading._hide();
+  }
 }.bind(this);
